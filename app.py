53cb334c30fb029fdbb6f61dc8e48f8edc43a960
joschonarth <joschonarth@gmail.com>
2024-10-28 23:27:40 -0300

    Create database

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
+@app.route("/hello-world", methods=['GET'])
+def hello_world():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
